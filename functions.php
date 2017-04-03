ca03e53e24378ebdca7a9adf2ec2e8d60d1abff4
Alex Devivye <aleksandr.devivye.1@my.csun.edu>
2017-04-03 14:09:20 -0700

    finished

--- functions.php
+++ functions.php
@@ -1,57 +1,66 @@
-<?php 
+<?php
 
-function titlecase($word) {
-  $word = ucwords($word);
-  return $word;
+function titlecase($word)
+{
+    $word = ucwords($word);
+    return $word;
 }
 
-function capfirst($word) {
-  $word = ucfirst($word);
-  return $word;
+function capfirst($word)
+{
+    $word = ucfirst($word);
+    return $word;
 }
 
-function scionPrice($theName, $scionModels, $theModel, $theQuantity) {
-  if ($theModel != 'nothing' && $theQuantity > 0) {
-    $valid = true;
-    $price = $scionModels[$theModel];
-    $total = $price / $theQuantity;
-    
-    if ($theQuantity < 2) {
-      $title = titlecase($theModel).' for '.$theName;
-      $theTotal = 'Total: $'.number_format($total, 2);
-      $description = $theName.' wants to buy '.$theModel.'.';
-    } 
+function scionPrice($theName, $scionModels, $theModel, $theQuantity)
+{
+    if ($theModel != 'nothing' && $theQuantity > 0) {
+        $valid = true;
+        $price = $scionModels[$theModel];
+        $total = $price / $theQuantity;
+        
+        if ($theQuantity == 1) {
+            $title       = titlecase($theModel) . ' for $' . $price;
+            $theTotal    = 'MSRP: $' . number_format($total, 2);
+            $description = $theName . ' wants to buy a Scion ' . $theModel;
 
-    elseif ($theQuantity > 50) {
-      $title = 'Sorry '.titlecase($theName).' we cannot split the order on ' .$thequantity 'transactions';
-      $theModel = 'Ooopps!';
-      $description = 'We&rsquo;re sorry,'.$theName.',thats too many cards for us to process maybe take alook at something less than $'.number_format($total, 2).'! Or maybe try Venmo!';
-    } 
-
-    else {
-      $title = .$theName 'is looking at ' titlecase($theModel).' for ';
-      $theTotal = 'Total: $'.number_format($total, 2)'.';
-      $description = $theName.' wants  '.number_format($theQuantity).' people to split the cost of the Scion'.$theModel.'.';
+        }
+        
+        elseif ($theQuantity > 10) {
+            $title       = 'Ooopps! ' ;
+            $theModel    = 'Oopps';
+           
+            $description = 'We&rsquo;re sorry, ' . $theName . ' ,that&rsquo;s too many cards for us to process, we cannot split the order on  '. $theQuantity . ' transactions, our limit is 10 cards. Please reduce the amount of credit cards or take a look at something less than $' . number_format($price, 2) . '! Or maybe try Venmo!';
+           
+        }
+        
+        else {
+            $title       = $theName . ' is looking at a Scion ' . titlecase($theModel) . ' for $' . $price;
+            $theTotal    = 'Scion ' . $theModel . ' at ' . $price . ' split on ' . number_format($theQuantity) . ' people is $' . number_format($total, 2) . ' per person.';
+            $title = $theName . ' wants  ' . number_format($theQuantity) . ' people to split the cost of the Scion' . $theModel . '.';
+        }
     }
-  } 
-
-  else {
-    $valid = false;
-  };
-
-  if ($valid == true) {
-    return('
+    
+    else {
+        $valid = false;
+    }
+    ;
+    
+    if ($valid == true) {
+        return ('
       <div class="card my-4 mx-auto" style="width: 20rem;">
-        <img class="img-fluid" src="images/'.$theModel.'.jpg" alt="Card image cap">
+        <img class="img-fluid" src="images/' . $theModel . '.jpg" alt="Card image cap">
         <div class="card-block">
-          <h2 class="h4 card-title">'.$title.'</h2>
-          <p class="card-text">'.$description.'</p>
-          <p class="h5">'.$theTotal.'</p>
+          <h2 class="h4 card-title">' . $title . '</h2>
+          <p1 class="card-text">' . $description . '</p1>
+          
+          
+          <p class="h5">' . $theTotal . ' </p>
         </div>
       </div>
     ');
-  } else {
-    return('
+    } else {
+        return ('
       <div class="alert alert-danger alert-dismissible fade show" role="alert">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
@@ -59,5 +68,5 @@ function scionPrice($theName, $scionModels, $theModel, $theQuantity) {
         <p class="m-0"><strong>You need to select a model</strong> </p>
       </div>
     ');
-  }
+    }
 }
--- index.php
+++ index.php
@@ -1,35 +1,34 @@
 <?php
-  // Load the functions
-  require_once('functions.php');
+// Load the functions
+require_once('functions.php');
 
-  // Scion prices array
-  $scionModels = array(
-  'frs'=>26255,
-  'ia'=>15700,
-  'im'=>18460,
-  'iq'=>16435,
-  'tc'=>19385,
-  'xa'=>12780,
-  'xb'=>16950,
-  'xd'=>15920
-  );
+// Scion prices array
+$scionModels = array(
+    'FR-S' => 26255.0,  
+    'iA' => 15700.0,
+    'iM' => 18460.0,
+    'iQ' => 16435.0,
+    'tC' => 19385.0,
+    'xA' => 12780.0,
+    'xB' => 16950.0,
+    'xD' => 15920.0
+);
 
-  // Process the form request
-  if( isset($_POST['submit']) )
-  {
-      $theName = htmlentities($_POST['name']);
-      $theModel = htmlentities($_POST['scion']);
-      $theModel = strtolower($theModel); // Lowercase form submission
-      $theQuantity = htmlentities($_POST['quantity']);
-      $scion = scionPrice($theName, $scionModels, $theModel, $theQuantity);
-  } 
+// Process the form request
+if (isset($_POST['submit'])) {
+    $theName     = htmlentities($_POST['name']);
+    $theModel    = htmlentities($_POST['scion']);
+    $theModel    = ($theModel); 
+    $theQuantity = htmlentities($_POST['quantity']);
+    $scion       = scionPrice($theName, $scionModels, $theModel, $theQuantity);
+}
 
-  else {
+else {
     // User hasn't entered a value
     $theModel = '';
-    $scion = '';
-    $theName = '';
-  }
+    $scion    = '';
+    $theName  = '';
+}
 ?>
 <!-- *******************************************************************************************************************************************************************************************endphp******************************************************************************************************************************************************************************************* -->
 
@@ -40,35 +39,46 @@
     <title>Split your Scion</title>
     <link rel="stylesheet" href="css/bootstrap.css">
   </head>
+
   <body class="bg-faded">
     <main class="container py-4">
       <h1 class="pb-4 font-weight-bold text-center">Whats your style?</h1>
       <form class="form-inline justify-content-center" action="" method="post">
         <label for="name" class="sr-only">Name</label>
         <input class="form-control mr-2" type="text" 
-        value="<?php echo ( $theName ? $theName : '' );?>" 
+        value="<?php
+echo ($theName ? $theName : '');
+?>" 
         placeholder="Your Name" name="name" id="name"> 
         <span class="mr-2">wants a</span>
         <select class="custom-select mr-2" name="scion" id="scion">
 
-          <?php echo '<option value="'.( $theModel ? $theModel : 'nothing' ).'">'.( $theModel ? capfirst($theModel) : 'Select a Model' ).'</option>'; ?>
 
-          <?php foreach ($scionModels as $model => $price) {
-            echo '<option value="'.$model.'">'.capfirst($model).'</option>';
-          }; ?>
+          <?php
+echo '<option value="' . ($theModel ? $theModel : 'nothing') . '">' . ($theModel ? ($theModel) : 'Select a Model') . '</option>';
+?>
+
+          <?php
+foreach ($scionModels as $model => $price) {
+    echo '<option value="' . $model . '">' . ($model) . '</option>';
+}
+;
+?>
 
         </select> 
 
         <span class="mr-2">split on </span>
-        <input class="form-control mr-2" type="number" value="<?php echo ( $theQuantity ? $theQuantity : '' );?>" placeholder="Quantity" name="quantity" id="quantity">
+        <input class="form-control mr-2" type="number" value="<?php
+echo ($theQuantity ? $theQuantity : '');
+?>" placeholder="Quantity" name="quantity" id="quantity">
         <button class="btn btn-outline-primary" name="submit" type="submit">Submit</button>
       </form>
 
-      <?php 
-        if($scion){
-           echo $scion;
-        }
-      ?>
+      <?php
+if ($scion) {
+    echo $scion;
+}
+?>
 
     </main>
 
